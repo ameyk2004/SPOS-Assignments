5e7f70e3d2d70d338f6806711a6bc873de582a7a
Amey Kulkarni <148298416+ameyk2004@users.noreply.github.com>
2024-11-12 08:00:21 +0530

    Assignment 6

--- Assignment_07/tcp_client.py
+++ Assignment_07/tcp_client.py
@@ -3,31 +3,36 @@ import threading
 
 HOST = '127.0.0.1'
 PORT = 3000
-
-def log(prefix, message:str):
+def log(prefix, message):
     print(f"[{prefix}]\t\t{message}")
 
-def run_client(client_id, message):
-    clientSocket = socket(AF_INET, SOCK_STREAM)
-    clientSocket.connect((HOST, PORT))
+def run_client(id, message):
+    client_socket = socket(AF_INET, SOCK_STREAM)
+    client_socket.connect((HOST, PORT))
 
-    log("INFO", f"\n{client_id} Connected to server")
-    log("INFO", f"Sending message : {message}")
+    log("INFO", f"Client {id} connected to server")
+    log("INFO", f"Sending message {message}")
 
-    clientSocket.sendall(message.encode())
-    data:bytes = clientSocket.recv(1024)
+    client_socket.sendall(message.encode())
 
-    log("INFO", f"Message Received from server: {data.decode()}")
-    clientSocket.close()
+    data = client_socket.recv(1024)
+    log("INFO", f"Recieved back message {data}")
+    
+    client_socket.close()
 
 def main():
 
-    messages = ["Message 1", "Message 2", "Message 3", "Message 4",]
+    messages = ["Hello There", "How are You"]
 
     for i in range(len(messages)):
-        client_thread = threading.Thread(target=run_client, args = (i+1, messages[i]))
+        message = messages[i]
+        client_thread = threading.Thread(target=run_client, args=(i+1, message))
         client_thread.start()
     
+    # client_socket = socket(AF_INET, SOCK_STREAM)
+    # client_socket.connect((HOST, PORT))
 
+    # log("INFO", "Client Connected")
+        
 if __name__ == "__main__":
     main()
--- Assignment_07/tcp_server.py
+++ Assignment_07/tcp_server.py
@@ -3,20 +3,18 @@ import threading
 
 HOST = '127.0.0.1'
 PORT = 3000
-
 def log(prefix, message):
     print(f"[{prefix}]\t\t{message}")
 
 def handle_client(client_socket: socket):
     while True:
-        data:bytes = client_socket.recv(1024)
+        
+        data = client_socket.recv(1024)
 
         if not data:
             break
-
-        log("INFO", f"Message from Client : {data.decode()}")
-        client_socket.send(data)
-
+        log("INFO", f"Data Recieved : {data.decode()}")
+        client_socket.sendall(data)
     client_socket.close()
 
 def main():
@@ -25,27 +23,15 @@ def main():
     serverSocket.bind((HOST, PORT))
     serverSocket.listen(10)
 
-    log("INFO", "listening on Port 3000")
+    log("INFO", "Listening on Port 3000")
 
     while True:
 
-        try:
-            clientSocket, client_addr = serverSocket.accept()
-            log("INFO", f"Client Connected : {client_addr[0]}:{client_addr[1]}")
-            client_thread = threading.Thread(target=handle_client, args=(clientSocket,))
-            client_thread.start()
+        client_socket, client_addr = serverSocket.accept()
+        log("INFO", f"Connected to Client : {client_addr[0]}")
 
-        except KeyboardInterrupt:
-            log("LOG", "Server Shutting Down...")
-            break
-        except Exception as e:
-            log("ERROR", "error occurred")
-            print(e)
-            break
-
-    serverSocket.close()  
-
-    
-
+        client_thread = threading.Thread(target=handle_client, args=(client_socket,))
+        client_thread.start()
+        
 if __name__ == "__main__":
     main()
